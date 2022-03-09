250b15a30ca7df87ed086d08202f8aaa3bb283b5
torijameson <vtjameson@gmail.com>
2022-03-09 09:15:11 -0600

    working on part 3 but HTML is screamy

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,8 +24,21 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @GetMapping("search/{model}")
     @ResponseBody
-    public String displaySearchResults (@PathVariable String model) {
+    //confused about where Model is / where I add atrribute of Model? I asked Elenora and she said you had to add model as an attribute elsewhere but that makes no sense to me
+    public String displaySearchResults (@PathVariable Model model){
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
 
+        if this.searchTerm.getValue().equals("all")||this.searchTerm.getValue().equals(""){
+            return JobData.findAll(<jobs>);
+        } else {
+            return JobData.findByColumnAndValue(<jobs>;
+                    }//isn't <> indicative of an array?
+
+<table>
+    <tr th:each="searchType, searchTerm : ${search/model}">
+            <td>
+                //provided below
     <form th:action = "@{/search}" method = "get" >
     <label > Name:
   <input type = "text" name = "keyword" th:
@@ -33,7 +47,14 @@ public class SearchController {
                 <div th:text="${param.q}">Search Query</div>
   <button type = "submit" class="btn btn-info" > Reset </button >
   </form >
+                //provided above
+                </td>
+                </tr>
+                </table>
 
     }
+
+
+    // I have no idea where to put the two passages into views. Here? but they are html? so confused.
 
 }
