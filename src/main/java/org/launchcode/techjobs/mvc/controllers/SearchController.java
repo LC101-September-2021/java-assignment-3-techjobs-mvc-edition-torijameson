e32cfc97f2f8359f72e984e0d9685206496b4622
torijameson <vtjameson@gmail.com>
2022-03-09 18:03:58 -0600

    Part 3 with html and thymeleaf errors

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
 import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -22,37 +25,37 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @GetMapping("search/{model}")
-    @ResponseBody
-    //confused about where Model is / where I add atrribute of Model? I asked Elenora and she said you had to add model as an attribute elsewhere but that makes no sense to me
-    public String displaySearchResults (@PathVariable Model model){
-        model.addAttribute("searchType", searchType);
-        model.addAttribute("searchTerm", searchTerm);
-
-        if this.searchTerm.getValue().equals("all")||this.searchTerm.getValue().equals(""){
-            return JobData.findAll(<jobs>);
+    @GetMapping("results")
+     public String displaySearchResults (@PathVariable Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+       ArrayList<Job> jobs;
+        if (searchTerm.equals("all")|| searchTerm.equals("")){
+            jobs=JobData.findAll();
         } else {
-            return JobData.findByColumnAndValue(<jobs>;
-                    }//isn't <> indicative of an array?
+           jobs=JobData.findByColumnAndValue(searchType, searchTerm);
+        }
 
-<table>
-    <tr th:each="searchType, searchTerm : ${search/model}">
-            <td>
-                //provided below
-    <form th:action = "@{/search}" method = "get" >
-    <label > Name:
-  <input type = "text" name = "keyword" th:
-        value = "${keyword}" placeholder = "Find by Owner Name & Shop Type" size = "50" >
-  <button type = "submit" class="btn btn-info" > Search </button >
-                <div th:text="${param.q}">Search Query</div>
-  <button type = "submit" class="btn btn-info" > Reset </button >
-  </form >
-                //provided above
-                </td>
-                </tr>
-                </table>
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
 
-
+        return "search";
+//<table>
+//    <tr th:each="searchType, searchTerm : ${search/model}">
+//            <td>
+//                //provided below
+//    <form th:action = "@{/search}" method = "get" >
+//    <label > Name:
+//  <input type = "text" name = "keyword" th:
+//        value = "${keyword}" placeholder = "Find by Owner Name & Shop Type" size = "50" >
+//  <button type = "submit" class="btn btn-info" > Search </button >
+//                <div th:text="${param.q}">Search Query</div>
+//  <button type = "submit" class="btn btn-info" > Reset </button >
+//  </form >
+//                //provided above
+//                </td>
+//                </tr>
+//                </table>
+//
+//
     }
 
 
